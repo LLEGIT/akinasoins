579e47774c44bd7ba938568dc7da56c467dfa794
Spamenpagaille <spamenpagaille@gmail.com>
2024-10-09 15:08:20 +0200

    correction coquille sur vue questionnaire

--- resources/views/questions.blade.php
+++ resources/views/questions.blade.php
@@ -1,6 +1,6 @@
 @extends('layout')
 
-@section('questions')
+@section('content')
 <div class="md:w-1/2 w-full bg-white p-6 rounded-lg shadow-md">
     <h2 class="text-2xl font-semibold mb-4">{{ $question['titre'] }}</h2>
 
